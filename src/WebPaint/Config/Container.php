041310983d9c898357dba5541c6fa62dc6210724
Anatoly Garkusha <agarkusha1989@gmail.com>
2014-05-07 21:53:20 +0400

    Modify class Config\Container added toArray() method

--- src/WebPaint/Config/Container.php
+++ src/WebPaint/Config/Container.php
@@ -40,4 +40,26 @@ class Container
     {
         return isset($this->data[$name]);
     }
+    
+    /**
+     * Method converts the internal representation of an array in php
+     * 
+     * @return array
+     */
+    public function toArray()
+    {
+        $array = array();
+        foreach ($this->data as $name => $value)
+        {
+            if ($value instanceof static)
+            {
+                $array[$name] = $value->toArray();
+            }
+            else
+            {
+                $array[$name] = $value;
+            }
+        }
+        return $array;
+    }
 }
