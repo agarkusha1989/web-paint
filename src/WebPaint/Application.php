c75ac75512aee699ba79098e89d366c9340829a9
Anatoly Garkusha <agarkusha1989@gmail.com>
2014-05-07 20:51:20 +0400

    Created blank Application class

---
+++ src/WebPaint/Application.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace WebPaint;
+
+class Application
+{
+    public function __construct()
+    {
+        spl_autoload_register(array($this, 'loadClass'));
+    }
+    
+    /**
+     * Method to load classes
+     * 
+     * @param string $class 
+     */
+    public function loadClass($class)
+    {
+        $filename = __DIR__ . '/' . str_replace("\\", '/', $class) . '.php';
+        if (file_exists($filename))
+        {
+            require_once $filename;
+        }
+    }
+    
+    /**
+     * The run application!
+     */
+    public function run()
+    {
+        echo 'Web paint is running!';
+    }
+}
