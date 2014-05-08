3cef577135d492793ec5ef3cc18a839c5c3cf897
Anatoly Garkusha <agarkusha1989@gmail.com>
2014-05-08 19:51:32 +0400

    Change js library

--- public_html/js/web-paint.js
+++ public_html/js/web-paint.js
@@ -3,38 +3,80 @@
  */
 var Paint = {
     "c2d": false,
-    "instrument": "bush",
-    "bgColor": "#000000",
-    "fgColor": "#ffffff",
     "isMouseDown": false,
-    "line": {
-        "startX": 0,
-        "startY": 0,
-        "color": "#000000",
-        "endX": 0,
-        "endY": 0
-    }
 };
 
-function paintInit()
+Paint.getInstrument = function()
+{
+    return $('#paint-instrument-current').text();
+}
+
+Paint.getBgColor = function()
+{
+    return $('#paint-color-bg').val();
+}
+
+Paint.getFgColor = function()
+{
+    return $('#paint-color-fg').val();
+}
+
+Paint.getLineWidth = function()
+{
+    return $('#paint-line-width').val();
+}
+
+Paint.getLineCap = function()
+{
+    return $('#paint-line-cap').val();
+}
+
+Paint.getRectangleType = function()
+{
+    return $('#paint-rectangle-type').val();
+}
+
+Paint.getRectangleBegin = function()
+{
+    return {
+        "x": $('#rect-x').val(),
+        "y": $('#rect-y').val()
+    };
+}
+
+Paint.setInstrument = function(instrument)
+{
+    $('#paint-instrument-current').text(instrument);
+}
+
+Paint.setRectangleBegin = function(begin)
+{
+    $('#rect-x').val(begin.x);
+    $('#rect-y').val(begin.y);
+}
+
+Paint.init = function()
 {
     Paint.c2d = document.getElementById('paint').getContext('2d');
     
+    $('#paint').parent().append(
+            '<input type="hidden" id="rect-x" />' +
+            '<input type="hidden" id="rect-y" />');
+    
+    $('#paint-instrument-brush').bind('click', function() { Paint.setInstrument('Brush'); });
+    $('#paint-instrument-line').bind('click', function() { Paint.setInstrument('Line'); });
+    $('#paint-instrument-rectangle').bind('click', function() { Paint.setInstrument('Rectangle'); });
     $('#paint').bind('mousemove', mousemoveAction);
     $('#paint').bind('mousedown', mousedownAction);
     $('#paint').bind('mouseup', mouseupAction);
-    $('#paint-file-new').bind('click', fileNewAction);
-    $('#paint-file-open').bind('click', fileOpenAction);
-    $('#paint-file-save').bind('click', fileSaveAction);
-    $('#paint-file-exit').bind('click', fileExitAction);
-    $('#paint-instrument-brush').bind('click', instrumentBrushAction);
-    $('#paint-instrument-line').bind('click', instrumentLineAction);
-    $('#paint-instrument-rectangle').bind('click', instrumentRectangleAction);
-    $('#paint-instrument-circle').bind('click', instrumentCircleAction);
-    $('#paint-color-bg').bind('change', colorBgAction);
-    $('#paint-color-fg').bind('change', colorFgAction);
-    $('#paint-color-bg').val(Paint.bgColor);
-    $('#paint-color-fg').val(Paint.fgColor);
+    //$('#paint-file-new').bind('click', fileNewAction);
+    //$('#paint-file-open').bind('click', fileOpenAction);
+    //$('#paint-file-save').bind('click', fileSaveAction);
+    //$('#paint-file-exit').bind('click', fileExitAction);
+    //$('#paint-instrument-brush').bind('click', instrumentBrushAction);
+    //$('#paint-instrument-line').bind('click', instrumentLineAction);
+    //$('#paint-instrument-rectangle').bind('click', instrumentRectangleAction);
+    //$('#paint-instrument-circle').bind('click', instrumentCircleAction);
 }
 
 // canvas actions
@@ -42,91 +84,90 @@ function mousemoveAction(event)
 {    
     if (Paint.isMouseDown)
     {
-        switch(Paint.instrument)
+        switch(Paint.getInstrument())
         {
-            case 'brush':
+            case 'Brush':
                 Paint.c2d.lineTo(
                         event.pageX - $('#paint').offset().left,
                         event.pageY - $('#paint').offset().top);
-                Paint.c2d.strokeStyle = Paint.bgColor;
+                Paint.c2d.strokeStyle = Paint.getBgColor();
+                Paint.c2d.lineWidth = Paint.getLineWidth();
+                Paint.c2d.lineCap = Paint.getLineCap();
                 Paint.c2d.stroke();
                 break;
         }
-    }
-    else
-    {
-        
     }
 }
 
 function mousedownAction(event)
 {
-    Paint.isMouseDown = true;
+    var x = event.pageX - $('#paint').offset().left;
+    var y = event.pageY - $('#paint').offset().top;
     
-    switch (Paint.instrument)
+    Paint.isMouseDown = true;
+
+    switch (Paint.getInstrument())
     {
-        case 'brush':
-            Paint.c2d.moveTo(
-                    event.pageX - $('#paint').offset().left,
-                    event.pageY - $('#paint').offset().top);
+        case 'Brush':
+            Paint.c2d.beginPath();
+            Paint.c2d.moveTo(x, y);
+            break;
+        case 'Line':
+            Paint.c2d.beginPath();
+            Paint.c2d.moveTo(x, y);
+            break;
+        case 'Rectangle':
+            Paint.c2d.beginPath();
+            Paint.setRectangleBegin({"x": x, "y": y});
             break;
     }
 }
 
 function mouseupAction(event)
 {
+    var x = event.pageX - $('#paint').offset().left;
+    var y = event.pageY - $('#paint').offset().top;
+    
     Paint.isMouseDown = false;
     
-    switch (Paint.instrument)
+    Paint.c2d.strokeStyle = Paint.getBgColor();
+    Paint.c2d.fillStyle = Paint.getFgColor();
+    Paint.c2d.lineWidth = Paint.getLineWidth();
+    Paint.c2d.lineCap = Paint.getLineCap();
+    
+    switch (Paint.getInstrument())
     {
-        case 'brush':
-            Paint.c2d.save();
+        case 'Brush':
+            Paint.c2d.closePath();
+            break;
+        case 'Line':
+            Paint.c2d.lineTo(x, y);
+            Paint.c2d.stroke();
+            Paint.c2d.closePath();
+            break;
+        case 'Rectangle':
+            var begin = Paint.getRectangleBegin();
+            var type  = Paint.getRectangleType();
+
+            if (type == 'stroke')
+            {
+                Paint.c2d.strokeRect(begin.x, begin.y, x-begin.x, y-begin.y);
+            }
+            else if (type == 'fill')
+            {
+                Paint.c2d.fillRect(begin.x, begin.y, x-begin.x, y-begin.y);
+            }
+            else if (type == 'rect')
+            {
+                Paint.c2d.strokeRect(begin.x, begin.y, x-begin.x, y-begin.y);
+                Paint.c2d.fillRect(begin.x, begin.y, x-begin.x, y-begin.y);
+            }
+            
+            Paint.c2d.closePath();
             break;
     }
 }
 
-// Main menu actions
-function fileNewAction(event)
-{
-    
-}
-function fileOpenAction(event)
-{
-    
-}
-function fileSaveAction(event)
-{
-    
-}
-function fileExitAction(event)
-{
-    
-}
-function instrumentBrushAction()
-{
-    Paint.instrument = "brush";
-}
-function instrumentLineAction(event)
-{
-    Paint.instrument = "line";
-}
-function instrumentRectangleAction(event)
-{
-    Paint.instrument = "rectangle";
-}
-function instrumentCircleAction(event)
-{
-    Paint.instrument = "circle";
-}
-function colorBgAction(event)
-{
-    Paint.bgColor = $('#paint-color-bg').val();
-}
-function colorFgAction(event)
-{
-    Paint.fgColor = $('#paint-color-fg').val();
-}
-
 $('document').ready(function() {
-    paintInit();
+    Paint.init();
 });
