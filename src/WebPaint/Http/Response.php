0b303793ef4619cf8be33821b396e15a06b928bf
Anatoly Garkusha <agarkusha1989@gmail.com>
2014-05-09 17:04:43 +0400

    Adding class Http\Response

---
+++ src/WebPaint/Http/Response.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Http;
+
+class Response
+{
+    protected $statusCode;
+    protected $content;
+    protected $headers;
+    
+    function __construct($statusCode = 200, $content = '', $headers = array())
+    {
+        $this->statusCode = $statusCode;
+        $this->content    = $content;
+        $this->headers    = $headers;
+    }
+
+    public function send()
+    {
+        if (function_exists('http_response_code'))
+        {
+            http_response_code($this->statusCode);
+        }
+        else
+        {
+            heaader('HTTP/1.1 ' . $this->statusCode);
+        }
+        
+        foreach ($this->headers as $header)
+        {
+            header($header);
+        }
+        
+        echo $this->content;
+    }
+}
